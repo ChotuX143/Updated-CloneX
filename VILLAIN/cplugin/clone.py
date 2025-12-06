f1382ba017e68cb9f857fb1f0340d3bdbffa099e
UFC_UPDATES <ufcupdates313@gmail.com>
2025-12-06 17:02:55 +0530

    Update clone.py

--- VILLAIN/cplugin/clone.py
+++ VILLAIN/cplugin/clone.py
@@ -12,7 +12,7 @@ from .utils import StartTime
 from VILLAIN.utils import get_readable_time
 from VILLAIN.utils.decorators.language import language
 
-APP_LINK = f"https://t.me/Pikaaclonebot"
+APP_LINK = f"https://t.me/TamannaCloneBot"
 
 
 @Client.on_message(filters.command("clone"))
