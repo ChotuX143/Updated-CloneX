3f70c25a12bfa7f3e2c71324669d853533bc13b7
Rishu <vishwakarmarishabh366@gmail.com>
2025-12-08 09:08:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,9 +18,9 @@ ASSUSERNAME = getenv("ASSUSERNAME", "")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 
-YTPROXY_URL = ""
-
-YT_API_KEY = ""
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## 
+YT_API_KEY = getenv("YT_API_KEY" , 'xbit_B4BJY2DSA1NEGXRRIGV9YZ') 
+COOKIES_URL=getenv("COOKIES_URL" , "https://gist.githubusercontent.com/sparrow9616/f29fc6588086a3c72d92dd9c03773350/raw/4229f3f4aab4a6693fc0794d136d30f54d67ae85/gistfile1.txt")
 
 
 API_URL = "https://api-dl-1.gleeze.com/yt"
