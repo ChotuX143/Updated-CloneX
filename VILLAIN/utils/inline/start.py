c8d80b678b7082d8e2ebb4018138252643f76db1
officialrohit928-lang <officialrohit928@gmail.com>
2026-03-26 15:56:38 +0530

    Update start.py

--- VILLAIN/utils/inline/start.py
+++ VILLAIN/utils/inline/start.py
@@ -5,43 +5,54 @@ import asyncio
 from VILLAIN import app
 
 
-# Start panel for inline buttons
 def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["SO_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            )],
-        [
-           
-
-InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-
-InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(
+                text=_["S_B_2"],
+                url=config.SUPPORT_CHAT
+            ),
         ],
     ]
     return buttons
 
 
 
-# Private panel for inline buttons
 def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT)
+                url=f"https://t.me/{app.username}?startgroup=true"
+            ),
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-InlineKeyboardButton(text="📨ʏᴛ-ᴀᴘɪ", callback_data=f"oapi"),
+            InlineKeyboardButton(
+                text="💌 ʏᴛ-ᴀᴘɪ",
+                callback_data="bot_info_data"
+            ),
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [
+            InlineKeyboardButton(
+                text=_["S_B_2"],
+                callback_data="clone_me"
+            ),
+            InlineKeyboardButton(
+                text="𓂃⃪𑪖𝐆𝛂ϻ𝛆𝐬",
+                callback_data="shiva"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["S_B_4"],
+                callback_data="settings_back_helper"
+            ),
+        ],
     ]
     return buttons
 
