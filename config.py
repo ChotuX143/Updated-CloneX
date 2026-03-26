d74a2c6055fd8598d3fdfc7f6edf423938ac7257
officialrohit928-lang <officialrohit928@gmail.com>
2026-03-26 17:55:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -52,7 +52,7 @@ GIT_TOKEN = getenv("GIT_TOKEN", "https://t.me/TamannaSupport")
 
 # --- Support ---
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ll_P_U_L_lI")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+JXzasmDva8c2Njhl")
 
 # --- Assistant ---
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
