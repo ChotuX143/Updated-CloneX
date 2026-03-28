0352cfac9c7fddd2f3d49c93f1ed59988f89f704
officialrohit928-lang <officialrohit928@gmail.com>
2026-03-28 11:25:39 +0530

    Update help.py

--- VILLAIN/utils/inline/help.py
+++ VILLAIN/utils/inline/help.py
@@ -90,7 +90,7 @@ def first_page(_):
             [
                 InlineKeyboardButton(text=_["C_B_1"], callback_data="help_callback chelp"),
             ],
-            [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")]
+            [InlineKeyboardButton(text="๏ ᴍᴇɴᴜ ๏", callback_data=f"settingsback_helper")]
         ]
     )
     return first_page_menu
